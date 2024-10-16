9e6be853c1135c92af1006dd1be38f78d5a03e79
Евгений Корзун <89513152520@yandex.ru>
2024-10-16 10:57:20 +0300

    initial commit

---
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/MainActivityTest.java
@@ -0,0 +1,34 @@
+package ru.kkuzmichev.simpleappforespresso;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import io.qameta.allure.android.runners.AllureAndroidJUnit4;
+
+@RunWith(AllureAndroidJUnit4.class)
+public class MainActivityTest {
+    private ViewInteraction mainText = onView(withId(R.id.text_home));
+
+
+    String HomeFragment = "This is home fragment";
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void mainActivityTest() {
+        mainText.check(matches(isDisplayed()));
+        mainText.check(matches(withText(HomeFragment)));
+    }
+}
